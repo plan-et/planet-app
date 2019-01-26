52e6c76599c8303f3aa59731e4c4de6965f377f8
m2vang <m.vang02@yahoo.com>
2019-01-26 15:03:34 -0600

    Routers are setup.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import LoginScreen from './components/loginScreen/loginScreen';
+import MainScreen from './components/mainScreen/mainScreen';
+import LandingScreen from './components/landingScreen/landingScreen';
+import CreateEventScreen from './components/createEventScreen/createEventScreen';
+import DiscoverScreen from './components/discoverScreen/discoverScreen';
+import LargeEventScreen from './components/largeEventScreen/largeEventScreen';
+import MyEventsScreen from './components/myEventsScreen/myEventsScreen';
+import OverviewScreen from './components/overviewScreen/overviewScreen';
+import SignupScreen from './components/signupScreen/signupScreen';
+import SmallEventScreen from './components/smallEventScreen/smallEventScreen';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Plan(et)
-          </p>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={LoginScreen} />
+          <Route path="/main" component={MainScreen} />
+          <Route path="/landing" component={LandingScreen} />
+          <Route path="/createEvent" component={CreateEventScreen} />
+          <Route path="/discover" component={DiscoverScreen} />
+          <Route path="/largeEvent" component={LargeEventScreen} />
+          <Route path="/myEvents" component={MyEventsScreen} />
+          <Route path="/overview" component={OverviewScreen} />
+          <Route path="/signup" component={SignupScreen} />
+          <Route path="/smallEvent" component={SmallEventScreen} />
+        </div>
+      </Router>
     );
   }
 }
