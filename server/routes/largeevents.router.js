e6e053ae62cb873a850976c692813c9054991ca2
Stephanie Ratanas <stephanieratanas@Stephanies-MacBook-Pro-2.local>
2019-01-27 11:41:13 -0600

    Added the best sql query ever

--- server/routes/largeevents.router.js
+++ server/routes/largeevents.router.js
@@ -23,14 +23,14 @@ router.get('/eventtypes', function (req, res) {
     });
 });
 
-//gets event by id minus the checklist
+//gets LARGE events by id minus the checklist
 router.get('/:id', function (req, res) {
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
             console.log('error', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
-            client.query(`SELECT * FROM small_events WHERE id = $1;`,[req.params.id], function (errorMakingDatabaseQuery, result) {
+            client.query(`SELECT * FROM large_events WHERE id = $1;`,[req.params.id], function (errorMakingDatabaseQuery, result) {
                 done();
                 if (errorMakingDatabaseQuery) {
                     console.log('error', errorMakingDatabaseQuery);
@@ -43,15 +43,15 @@ router.get('/:id', function (req, res) {
     });
 });
 
-//gets event checklist by event id
+//gets event checklist by event id for large event
 router.get('/:id/checklist', function (req, res) {
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
             console.log('error', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
-            client.query(`SELECT DISTINCT A.item FROM checklist_small_events as A
-            JOIN checklist_small_events_small_event as B
+            client.query(`SELECT DISTINCT A.item FROM checklist_large_events as A
+            JOIN checklist_large_events_large_event as B
             ON A.id = B.checklist_id
             WHERE B.event_id=$1;`,[req.params.id], function (errorMakingDatabaseQuery, result) {
                 done();
@@ -66,27 +66,41 @@ router.get('/:id/checklist', function (req, res) {
     });
 });
 
-
-
-let newSmallEventId;
-//Adding a new event, need to add other fields and connect to organization.
-router.post('/create/smallevent', function (req, res) {
+let newLargeEventId;
+//Adding a new event, WITH checklist!!!!
+router.post('/create', function (req, res) {
+    
     const query = 
-    `WITH small_events_entry as(
-                   INSERT INTO "small_events" (event_name, date, start_time, end_time, description, public, event_type)
-                   VALUES($1, TO_DATE($2, 'Mon DD YYYY'), to_char($3::time, 'HH12:MI AM'), to_char($4::time, 'HH12:MI AM'), $5, $6, $7)
-                   RETURNING id)
-                   INSERT INTO organizers_small_events (user_id , small_event_id) 
-                   VALUES($8,(SELECT id from small_events_entry));`;
-    var newSmallEvent = req.body;
+`WITH large_events_entry as(
+                    INSERT INTO "large_events" (event_name, date, start_time, end_time, description, public, event_type)
+                    VALUES($1, TO_DATE($2, 'Mon DD YYYY'), to_char($3::time, 'HH12:MI AM'), to_char($4::time, 'HH12:MI AM'), $5, $6, $7)
+                   RETURNING id),
+ organizers_large_events_entry as (
+                    INSERT INTO organizers_large_events (user_id , large_event_id) 
+                    VALUES($8,(SELECT id from large_events_entry))
+                    RETURNING id)
+                    INSERT INTO checklist_large_events_large_event (event_id, checklist_id, haveneedna) 
+                    VALUES 
+                    ((SELECT id from large_events_entry),1,$9),
+                    ((SELECT id from large_events_entry),2,$10),
+                    ((SELECT id from large_events_entry),3,$11),
+                    ((SELECT id from large_events_entry),4,$12),
+                    ((SELECT id from large_events_entry),5,$13),
+                    ((SELECT id from large_events_entry),6,$14),
+                    ((SELECT id from large_events_entry),7,$15),
+                    ((SELECT id from large_events_entry),8,$16),
+                    ((SELECT id from large_events_entry),9,$17);`;
+    var newLargeEvent = req.body;
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
             console.log('error', errorConnectingToDatabase);
             res.sendStatus(500);
 
         } else {
-            client.query(query , [newSmallEvent.event_name, newSmallEvent.date, newSmallEvent.start_time, 
-                newSmallEvent.end_time, newSmallEvent.description, newSmallEvent.public, newSmallEvent.event_type, newSmallEvent.user_id],
+            client.query(query , [newLargeEvent.event_name, newLargeEvent.date, newLargeEvent.start_time, 
+                newLargeEvent.end_time, newLargeEvent.description, newLargeEvent.public, newLargeEvent.event_type, newLargeEvent.user_id,
+                newLargeEvent.checklistItems[0],newLargeEvent.checklistItems[1],newLargeEvent.checklistItems[2],newLargeEvent.checklistItems[3],newLargeEvent.checklistItems[4],
+                newLargeEvent.checklistItems[5],newLargeEvent.checklistItems[6],newLargeEvent.checklistItems[7],newLargeEvent.checklistItems[9]],
                 function (errorMakingDatabaseQuery, result) {
                     done();
                     if (errorMakingDatabaseQuery) {
@@ -95,38 +109,6 @@ router.post('/create/smallevent', function (req, res) {
 
                     } else {
                         res.sendStatus(201);
-                        newSmallEventId = result.rows[0].id
-                        console.log(newSmallEventId)
-                    }
-                })
-        }
-    })
-})
-
-
-//creates checklist for last added event, $2 is the id of the checklist_small_event item (hackey i know)
-router.post('/create/smallevent/checklist', function (req, res) {
-    const query = 
-    `INSERT INTO checklist_small_events_small_event (event_id, checklist_id)
-    VALUES ($1,$2),($1,$2)`;
-    var smallEventChecklist = req.body;
-    pool.connect(function (errorConnectingToDatabase, client, done) {
-        if (errorConnectingToDatabase) {
-            console.log('error', errorConnectingToDatabase);
-            res.sendStatus(500);
-
-        } else {
-            client.query(query, [newSmallEventId, smallEventChecklist.id],
-                function (errorMakingDatabaseQuery, result) {
-                    done();
-                    if (errorMakingDatabaseQuery) {
-                        console.log('error', errorMakingDatabaseQuery);
-                        res.sendStatus(500);
-
-                    } else {
-                        res.sendStatus(201);
-                        newSmallEventId = result.rows[0].id
-                        console.log(newSmallEventId)
                     }
                 })
         }
@@ -135,3 +117,4 @@ router.post('/create/smallevent/checklist', function (req, res) {
 
 
 module.exports = router
+
